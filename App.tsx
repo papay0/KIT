9d21b59c71df10eb2b79b3f8f3abd0f7b83a90eb
Arthur Papailhau <papailhau@uber.com>
2020-03-08 17:49:40 -0700

    Adds firebase

--- App.tsx
+++ App.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+import ApiKeys from "./app/constants/ApiKeys";
+import * as firebase from "firebase/app";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    if (!firebase.apps.length) {
+      firebase.initializeApp(ApiKeys.FirebaseConfig);
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
